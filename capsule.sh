53eae26727f988d63c1a5ea0af164d989a5c482f
prashanth thipparthi <prth3635@colorado.edu>
2019-12-14 17:56:39 -0500

    network namespace

--- capsule.sh
+++ capsule.sh
@@ -3,7 +3,7 @@ sudo cgset -r cpu.cfs_period_us=1000000 container
 sudo cgset -r cpu.cfs_quota_us=100000 container
 
 sudo cgcreate -g memory:/container
-sudo cgset -r memory.limit_in_bytes=2000000 container
+sudo cgset -r memory.limit_in_bytes=30000000 container
 
 sudo cgcreate -g blkio:/container
 sudo cgset -r blkio.throttle.read_bps_device="259:0 1000000" container
